0eba9750c2f81fe7bb74ee4d2db8acde12ecb38e
Joppy Furr <joppyfurr@gmail.com>
2020-05-08 16:15:38 +1200

    Add some package installation to the setup script

--- setup.sh
+++ setup.sh
@@ -12,10 +12,50 @@ then
     exit
 fi
 
+echo
+echo "  ╭────────────────────────╮"
+echo "  │  Snepper Setup Script  │"
+echo "  ╰────────────────────────╯"
+echo
+
+echo "Package Installation:"
+
+PACKAGES="
+    build-essential
+    curl
+    git
+    htop
+    keepassxc
+    kpcli
+    suckless-tools
+    vim
+    vim-gtk
+    xfonts-terminus
+    zsh"
+
+for PACKAGE in ${PACKAGES}
+do
+    if dpkg -s ${PACKAGE} > /dev/null 2> /dev/null
+    then
+        echo " -> ${PACKAGE} is already installed."
+    else
+        echo " -> Installing ${PACKAGE}"
+        sudo apt-get install ${PACKAGE}
+    fi
+done
+echo
+
 # Vim
-ln -s ${HOME}/Dotfiles/vimrc ${HOME}/.vimrc
+echo "Vim Setup:"
+ln -s ${HOME}/Dotfiles/vimrc ${HOME}/.vimrc 2>/dev/null
 mkdir -p ${HOME}/.vim/colors
-ln -s ${HOME}/Dotfiles/wombat.vim ${HOME}/.vim/colors/
+mkdir -p ${HOME}/.vim/pack/plugins/start
+ln -s ${HOME}/Dotfiles/wombat.vim ${HOME}/.vim/colors/ 2>/dev/null
+echo "Install Vim Lightline with:"
+echo "  'git clone https://github.com/itchyny/lightline.vim ~/.vim/pack/plugins/start/lightline'"
+echo "Install VimWiki with:"
+echo "  'git clone https://github.com/vimwiki/vimwiki.git ~/.vim/pack/plugins/start/vimwiki'"
+echo
 
 # Zsh
-ln -s ${HOME}/Dotfiles/zshrc ${HOME}/.zshrc
+ln -s ${HOME}/Dotfiles/zshrc ${HOME}/.zshrc 2>/dev/null
